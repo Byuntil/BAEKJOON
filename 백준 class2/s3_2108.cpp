b8c1736913250530fc4936dd285136dfb3173999
Byuntil <gugugu2014@jbnu.ac.kr>
2022-05-01 16:16:08 +0900

    add: 백준2108

--- 백준 class2/s3_2108.cpp
+++ 백준 class2/s3_2108.cpp
@@ -1,15 +1,45 @@
 #include<iostream>
 #include<algorithm>
-#include<vector>
+#include<cmath>
 
 using namespace std;
 
-void input() {
+int coordinate[500001];
+int cnt[8001];
+int N, a, b, c, d;
+int sum = 0;
 
+void input() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> coordinate[i];
+        sum += coordinate[i];
+        cnt[coordinate[i] + 4000]++;
+    }
 }
 
 void solution() {
+    sort(coordinate, coordinate + N);
 
+    int flag;
+    int max = 0;
+    for (int i = 0; i < 8001; i++) {
+        if (cnt[i] > max) {
+            max = cnt[i];
+            flag = i;
+        }
+    }
+
+    for (int i = flag + 1; i < 8001; i++) {
+        if (cnt[i] == max) {
+            flag = i;
+            break;
+        }
+    }
+    a = round(double(sum) / N);
+    b = coordinate[(N - 1) / 2];
+    c = flag - 4000;
+    d = coordinate[N - 1] - coordinate[0];
 }
 
 void solve() {
@@ -18,7 +48,10 @@ void solve() {
 }
 
 void printResult() {
-
+    cout << a << "\n";
+    cout << b << "\n";
+    cout << c << "\n";
+    cout << d << "\n";
 }
 
 int main() {

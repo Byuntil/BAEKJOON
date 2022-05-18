79701523582ae88332a37ecb8354e1d1d6cdac0d
Byuntil <gugugu2014@jbnu.ac.kr>
2022-05-18 23:34:25 +0900

    add: 백준9375

---
+++ 백준 class3/s3_9375.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<algorithm>
+#include<map>
+
+using namespace std;
+
+int T;
+
+
+void input() {
+    cin >> T;
+}
+
+void solution() {
+    int n;
+    string clothes, clothesType;
+    for (int i = 0; i < T; i++) {
+        map<string, int> ma;
+        cin >> n;
+        for (int j = 0; j < n; j++) {
+            cin >> clothes >> clothesType;
+            if (ma.find(clothesType) == ma.end()) {
+                ma.insert({clothesType, 1});
+            } else {
+                ma[clothesType]++;
+            }
+        }
+        int ans = 1;
+        for (auto a: ma) {
+            ans *= (a.second + 1);
+        }
+        ans -= 1;
+        cout << ans << "\n";
+    }
+}
+
+void solve() {
+    input();
+    solution();
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solve();
+    return 0;
+}

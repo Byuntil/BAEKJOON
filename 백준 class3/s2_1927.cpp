2491dee690b6435d3c86225db8940000baa28926
Byuntil <gugugu2014@jbnu.ac.kr>
2022-05-31 00:13:39 +0900

    add: 백준1927

---
+++ 백준 class3/s2_1927.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<algorithm>
+#include<queue>
+
+using namespace std;
+
+int N;
+priority_queue<int, vector<int>, greater<>> pq;
+
+void input() {
+    cin >> N;
+}
+
+void solution() {
+    int x;
+    for (int i = 0; i < N; i++) {
+        cin >> x;
+        if (x == 0) {
+            if (pq.size()) {
+                cout << pq.top() << "\n";
+                pq.pop();
+            } else {
+                cout << 0 << "\n";
+            }
+        } else {
+            pq.push(x);
+        }
+    }
+}
+
+void solve() {
+    input();
+    solution();
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solve();
+    return 0;
+}
